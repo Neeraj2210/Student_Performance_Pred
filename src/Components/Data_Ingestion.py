57bdf912e5e615efdccfe2723caa8f66c7267ff2
Neeraj Sahu <neerajsahu1022.com>
2023-08-24 13:48:31 +0530

    Model Trainer

--- src/Components/Data_Ingestion.py
+++ src/Components/Data_Ingestion.py
@@ -7,6 +7,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.Components.Data_Transformation import DataTransformation
+from src.Components.Data_Transformation import DataTransformationConfig
+
+from src.Components.Model_trainer import ModelTrainerConfig
+from src.Components.Model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -34,7 +40,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -45,5 +51,12 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
+
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
