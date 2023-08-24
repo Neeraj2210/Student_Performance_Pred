7c5c57cd0d7d661bab6b3f5fe3eec27c6832d6d0
Neeraj Sahu <neerajsahu1022.com>
2023-08-24 23:23:04 +0530

    Development Completed

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,9 @@ from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
+from sklearn.model_selection import GridSearchCV
+
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -23,14 +26,20 @@ def save_object(file_path, obj):
     
 
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+            para=param[list(models.keys())[i]]
 
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train)  # Train model
 
@@ -48,4 +57,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e, sys)
-        
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
